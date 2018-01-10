f58c51d1c45c5bb4a44766d9c3d87338532207e4
Marko <marko.r@enginee.rs>
2018-01-10 15:12:07 +0100

    animate sideNav

--- src/App.js
+++ src/App.js
@@ -3,18 +3,44 @@ import './App.css';
 import logo from './logo.svg';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      sideNav: 'sideNav',
+      app: 'app'
+    }
+
+    this.navBars = this
+      .navBars
+      .bind(this);
+  }
+  navBars() {
+    if (this.state.sideNav === 'sideNavShow') {
+      this.setState({sideNav: 'sideNavHide', app: 'appSlideIn'});
+    } else {
+      this.setState({sideNav: 'sideNavShow', app: 'appSlideOut'});
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header>
-          <button className="nav" href="#">
-          <i className="fa fa-bars fa-2x"></i>
-          </button>
-          <img className="logo" src={logo} alt="logo"/>
-        </header>
-        <main>
-          <h1>work</h1>
-        </main>
+      <div className="appRoot">
+        <div className={this.state.app}>
+          <header>
+            <button className="nav" onClick={this.navBars}>
+              <i className="fa fa-bars fa-2x"></i>
+            </button>
+            <img className="logo" src={logo} alt="logo"/>
+            <div className="nav"></div>
+          </header>
+          <main>
+            <h1>Main</h1>
+          </main>
+        </div>
+        <div className={this.state.sideNav}>
+          <h1>sideNav</h1>
+        </div>
       </div>
     );
   }
