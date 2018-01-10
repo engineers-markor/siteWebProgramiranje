ea98b7052fd0cdf921eda06fefea97699930b423
Marko <marko.r@enginee.rs>
2018-01-10 10:00:33 +0100

    Clean init react template.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <button className="nav" href="#">
+          <i className="fa fa-bars fa-2x"></i>
+          </button>
+          <img className="logo" src={logo} alt="logo"/>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <h1>work</h1>
+        </main>
       </div>
     );
   }
