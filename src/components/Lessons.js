a1bc9670daab9762fb2aa875ee9b0a3686ec453a
engineers-markor <marko.r@enginee.rs>
2018-01-15 11:59:26 +0100

    added sidebars for lessons

--- src/components/Lessons.js
+++ src/components/Lessons.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {getCoursesLessons} from '../base';
 import Loading from './Loading';
+import {Link, Route} from 'react-router-dom';
 
 export default class Lessons extends Component {
     constructor(props) {
@@ -31,8 +32,15 @@ export default class Lessons extends Component {
 
         console.log(this.state);
         return (
-            <div>
-                <h1>Course {this.state.course.name}</h1>
+            <div className="flexLesson">
+                <div className="contentLessons">
+                    <h1>Course {this.state.course.name}</h1>
+                </div>
+                <div className="navLessons">
+                    <Link to={'/courses/' + this.props.id + '/1'}>Lessons 1</Link>
+                    <Link to={'/courses/' + this.props.id + '/2'}>Lessons 2</Link>
+                    <Link to={'/courses/' + this.props.id + '/3'}>Lessons 3</Link>
+                </div>
             </div>
         )
     }
